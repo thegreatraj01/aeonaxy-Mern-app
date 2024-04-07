022a8004f688dd8453da9de8380ee3930e43dbf4
thegreatraj01 <thegreatraj0810@gmail.com>
2024-04-08 01:33:13 +0530

    updated upload function of multer

--- Server/Middleware/Multer.middleware.js
+++ Server/Middleware/Multer.middleware.js
@@ -1,11 +1,16 @@
-const path = require('path');
+import path from 'path';
+import multer from 'multer';
 
 const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
+    destination: (req, file, cb) => {
         cb(null, path.join(__dirname, 'temp_upload'));
     },
-    filename: function (req, file, cb) {
+    filename: (req, file, cb) => {
         const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
         cb(null, file.fieldname + '-' + uniqueSuffix + file.originalname);
     }
 });
+
+const upload = multer({ storage: storage }).single('profilePic');
+
+export default upload;
