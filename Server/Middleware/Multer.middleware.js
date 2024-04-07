4df44df672b7c9ad160a2220c871d66a56d589ec
thegreatraj01 <thegreatraj0810@gmail.com>
2024-04-08 01:25:17 +0530

    changed file path for multer

--- Server/Middleware/Multer.middleware.js
+++ Server/Middleware/Multer.middleware.js
@@ -1,13 +1,11 @@
-import multer from 'multer';
+const path = require('path');
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, './temp_upload/')
+        cb(null, path.join(__dirname, 'temp_upload'));
     },
     filename: function (req, file, cb) {
-        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
         cb(null, file.fieldname + '-' + uniqueSuffix + file.originalname);
     }
-})
-
-export const upload = multer({ storage: storage });
+});
