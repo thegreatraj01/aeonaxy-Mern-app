96eca929cc30e1b47f8110050bd870beac2bfb6f
thegreatraj01 <thegreatraj0810@gmail.com>
2024-04-08 01:51:44 +0530

    undo all changes

--- Server/Middleware/Multer.middleware.js
+++ Server/Middleware/Multer.middleware.js
@@ -1,14 +1,13 @@
-import path from 'path';
 import multer from 'multer';
 
 const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, path.join(__dirname, 'temp_upload'));
+    destination: function (req, file, cb) {
+        cb(null, './temp_upload/')
     },
-    filename: (req, file, cb) => {
-        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+    filename: function (req, file, cb) {
+        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
         cb(null, file.fieldname + '-' + uniqueSuffix + file.originalname);
     }
-});
+})
 
 export const upload = multer({ storage: storage });
