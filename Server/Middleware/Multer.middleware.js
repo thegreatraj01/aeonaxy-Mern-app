579d35717f067ac36aec92a3673eb81d34501379
thegreatraj01 <thegreatraj0810@gmail.com>
2024-04-08 19:08:03 +0530

    using fs module to save files

--- Server/Middleware/Multer.middleware.js
+++ Server/Middleware/Multer.middleware.js
@@ -1,28 +1,16 @@
-// import multer from 'multer';
-
-// const storage = multer.diskStorage({
-//     destination: function (req, file, cb) {
-//         cb(null, './temp_upload/')
-//     },
-//     filename: function (req, file, cb) {
-//         const uniqueSuffix = Date.now();
-//         cb(null, file.fieldname + '-' + uniqueSuffix + file.originalname);
-//     }
-// })
-
-// export const upload = multer({ storage: storage });
-
 import multer from 'multer';
-import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
-const uploadDirectory = './temp_upload/';
-console.log(fs.existsSync(uploadDirectory));
-console.log(!fs.existsSync(uploadDirectory));
+// Get the directory path of the current module
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+// console.log(__filename, __dirname);
 
-// Create the directory if it doesn't exist
-if (!fs.existsSync(uploadDirectory)) {
-    fs.mkdirSync(uploadDirectory);
-}
+const uploadDirectory = path.resolve(__dirname, '../temp_upload');
+
+
+console.log(uploadDirectory);
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
